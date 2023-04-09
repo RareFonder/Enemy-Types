232202c8c062c0994446869d78c95838ab079c58
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 17:09:27 +0800

    1 file modified

--- script.js
+++ script.js
@@ -5,43 +5,58 @@ window.onload = () => {
   canvas.height = 800;
 
   class Game {
-    constructor() {
+    constructor(ctx, width, height) {
+      this.ctx = ctx;
+      this.width = width;
+      this.height = height;
       this.enemies = [];
-      this.#addNewEnemy();
+      this.enemiesInterval = 1000;
+      this.enemyTimer = 0;
     }
-    update() {
+    update(deltaTime) {
+      this.enemies = this.enemies.filter(object => !object.markedForDeletion);
+      if (this.enemyTimer > this.enemiesInterval) {
+        this.#addNewEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
       this.enemies.forEach(object => object.update());
     }
     draw() {
-      this.enemies.forEach(object => object.update());
+      this.enemies.forEach(object => object.draw(this.ctx));
     }
     #addNewEnemy() {
-      this.enemies.push(new Enemy());
+      this.enemies.push(new Enemy(this));
     }
   };
 
   class Enemy {
-    constructor() {
-      this.x = 100;
-      this.y = 100;
+    constructor(game) {
+      this.game = game;
+      this.x = this.game.width;
+      this.y = Math.random() * this.game.height;
       this.width = 100;
-      this.height = 100;
+      this.height = 100;  
+      this.markedForDeletion = false;
     }
     update() {
       this.x--;
+
+      if (this.x < 0 - this.width) this.markedForDeletion = true;
     }
-    draw() {
+    draw(ctx) {
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
   };
 
-  const game = new Game();
+  const game = new Game(ctx, canvas.width, canvas.height);
   let lastTime = 1;
   const animate = (timeStamp) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    game.update();
+    game.update(deltaTime);
     game.draw();
 
     requestAnimationFrame(animate);
