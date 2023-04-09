856d06f6cb871fd9332e2e44fd0f0f11c4b02de0
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-09 16:38:41 +0800

    3 files modified

---
+++ script.js
@@ -0,0 +1,37 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 800;
+
+class Game {
+  constructor() {
+    this.enemies = [];
+  }
+  update() {
+
+  }
+  draw() {
+
+  }
+  #addNewEnemy() {
+
+  }
+}
+
+class Enemy {
+  constructor() {
+
+  }
+  update() {
+
+  }
+  draw() {
+
+  }
+}
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  requestAnimationFrame(animate);
+}
